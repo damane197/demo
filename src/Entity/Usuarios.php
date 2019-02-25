0d1d09a4cbab7a8f1ce9d6c2c5e409827e68a7d9
Daniel <damane197@gmail.com>
2019-02-25 14:02:16 +0100

    Añadido entidad Usuarios

---
+++ src/Entity/Usuarios.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: undanet
+ * Date: 25/02/19
+ * Time: 13:47
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="usuarios")
+ */
+class Usuarios
+{
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    public $name;
+
+}
